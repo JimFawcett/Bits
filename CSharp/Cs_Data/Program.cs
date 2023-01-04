25f5c1fb0cb84b6add084e38d2a880a10894ed29
Jim Fawcett <fawcettjim@outlook.com>
2023-01-04 13:56:06 -0600

    restructured C# code

--- CSharp/Cs_Data/Program.cs
+++ CSharp/Cs_Data/Program.cs
@@ -1,13 +1,17 @@
 ﻿/* Cs_Data::Cs_Data.cs */
 /*
   Static Data Types:
-    sbyte, byte, short, int, long, ushort, uint, ulong
-    float, double
-    decimal, char, bool
-    object, string
-    pointer
+    value types => assignment copies value
+    --------------------------------------
+      sbyte, byte, short, int, long, ushort, uint, ulong
+      float, double
+      decimal, char, bool
+      struct
+      Reference types => assignment copies reference
+    ----------------------------------------------
+      object, string, class
 */
-#pragma warning disable CS8602  // possibly null reference warning
+//#pragma warning disable CS8602  // possibly null reference warning
 // https://stackoverflow.com/questions/8173239/c-getting-size-of-a-value-type-variable-at-runtime/8173293#8173293
 // 
 using System;
@@ -17,12 +21,12 @@ using System.Reflection.Emit;
 namespace HelloData
 {
   /*-- 
-    T4 is a value type used for type demo example.
+    Svt is a value type used for type demo example.
     It consists of three public properties.
   --*/
-  public struct T4   // value type
+  public struct Svt   // value type with value type members
   { 
-    public T4() {}
+    public Svt() {}
     public int I { get; set; } = 0;
     public double D { get; set; } = 1.0;
     public char C { get; set; }  = 'a';
@@ -31,12 +35,12 @@ namespace HelloData
     }
   }
   /*-- 
-    T4 is a value type used for type demo example.
+    Crt is a value type used for type demo example.
     It consists of three public properties.
   --*/
-  public struct T5   // value type with reference type member
+  public class Crt   // reference type with a reference type member
   { 
-    public T5() {}
+    public Crt() {}
     public int I { get; set; } = 0;
     public double D { get; set; } = 1.0;
     public string S { get; set; }  = "a string";
@@ -47,20 +51,109 @@ namespace HelloData
   /*-- type demonstration --*/
   class Program
   {
-    /*-- simple reflection --*/
-    public void showType<T>(T t, String s)
+    /*-- demonstration begins here --*/
+    static void Main(string[] args)
     {
+      print(" Demonstrate C# types");
+      label("int - value type");
+      showOp("int t1 = 42");
+      int t1 = 42;
+      showType(t1, "t1");
+      iterate(t1);
+      print();
+      showOp("int t1a = t1");
+      int t1a = t1;
+      isSameObj(t1a, "t1a", t1, "t1");
+
+      // reference behavior - copy on write
+      label("string - reference type");
+      showOp("string t2 = \"a string\"");
+      string t2 = "a string";
+      showType(t2, "t2");
+      showNote("string has many methods - uncomment next line to see them");
+      //iterate(t2);
+      showOp("string t3 = t2");
+      string t3 = t2;
+      showType(t3, "t3");
+      isSameObj(t3,"t3",t2,"t2");
+
+      showOp("t3 += \" is here\"");
+      t3 += " is here";
+      showType(t3, "t3");
+      isSameObj(t3,"t3",t2,"t2");
+      
+      label("Object - base reference type");
+      showOp("Object obj1 - new Object()");
+      Object obj1 = new Object();
+      // showIdent(obj, "obj");
+      showType(obj1, "obj");
+      iterate(obj1);
+      showOp("Object obj2 = obj1");
+      Object obj2 = obj1;
+      isSameObj(obj2, "obj2", obj1, "obj1");
+
+      label("Svt Struct value type");
+      showOp("Svt t4 = new Svt()");
+      Svt t4 = new Svt();
+      t4.I = 3;
+      t4.D = 3.1415927;
+      t4.C = 'z';
+      t4.printSelf("t4");
+      showType(t4, "t4");
+      showNote("value type: size of object in stackframe");
+      iterate(t4);
+
+      showOp("Svt t4a = t4");
+      Svt t4a = t4;
+      t4a.printSelf("t4a");
+      t4.printSelf("t4");
+      isSameObj(t4a,"t4a",t4,"t4");
+      showType(t4a, "t4a");
+
+      showOp("t4a.C = 'q'");
+      t4a.C = 'q';
+      t4a.printSelf("t4a");
+      t4.printSelf("t4");
+      isSameObj(t4, "t4", t4a, "t4a");
+
+      label("Crt - ref type with ref member");
+      showOp("Crt t5 = new Crt()");
+      Crt t5 = new Crt();
+      t5.S = "SomeString";
+      t5.printSelf("t5");
+      showType(t5, "t5");
+      showNote("ref type: size of handle to object in heap");
+
+      showOp("Crt t5a = t5");
+      Crt t5a = t5;
+      t5a.printSelf("t5a");
+      isSameObj(t5a, "t5a", t5, "t5");
+      isSameObj(t5a.S, "t5a.S", t5.S, "t5.S");
+
+      showOp("t5a.S = \"new string\"");
+      t5a.S = "new string";
+      t5a.printSelf("t5a");
+      t5a.printSelf("t5");
+      isSameObj(t5a, "t5a", t5, "t5");
+      isSameObj(t5a.S, "t5a.S", t5.S, "t5.S");
+      showNote("source t5 was altered!");
+      Console.WriteLine("\nThat's all Folks!\n");
+    }
+    /*-- simple reflection --*/
+    public static void showType<T>(T t, String s)
+    {
+      #pragma warning disable CS8602  // possibly null reference warning
       Type tt = t.GetType();
       Console.WriteLine("{0}, {1}",tt.Name, s);
       int size = Utils.GetManagedSize(tt);
       Console.WriteLine("value: {0}, size: {1}", t, size);
     }
     /*-- beware, two distinct objects may have same hashcode --*/
-    public void showIdent<T>(T t, String n) {
+    public static void showIdent<T>(T t, String n) {
       int id = t.GetHashCode();
       Console.WriteLine("{0}, {1}", n, id);
     }
-    public void isSameObj<T>(T t1, String n1, T t2, String n2) {
+    public static void isSameObj<T>(T t1, String n1, T t2, String n2) {
       if(ReferenceEquals(t1, t2)) {
         Console.WriteLine("{0} is same object as {1}", n1, n2);
       }
@@ -68,8 +161,11 @@ namespace HelloData
         Console.WriteLine("{0} is not same object as {1}", n1, n2);
       }
     }
+    public static void showOp(string op) {
+      Console.WriteLine("--- {0} ---", op);
+    }
     // https://stackoverflow.com/questions/7613782/iterating-through-struct-members
-    public void iterate<T>(T t) where T:new() {
+    public static void iterate<T>(T t) /*where T:new()*/ {
       Console.WriteLine("fields:");
       foreach(
         var field in typeof(T).GetFields(
@@ -91,106 +187,16 @@ namespace HelloData
         );
       }
     }
-    public static void print(String s) {
+    public static void print(String s = "") {
       Console.WriteLine(s);
     }
-    public static void separator() {
-      Console.WriteLine("----------");
+    public static void label(string s) {
+      Console.WriteLine(
+        "\n---------------\n  {0}\n---------------", s
+      );
     }
-    /*-- demonstration begins here --*/
-    static void Main(string[] args)
-    {
-      Console.WriteLine(" Demonstrate C# types");
-      Console.WriteLine("----------------------");
-      print("   Double value type");
-      print("----------");
-      Program p = new Program();
-      print("--- double t1 = 3.5 ---");
-      double t1 = 3.5;
-      //p.showIdent(t1, "t1");
-      p.showType(t1, "t1");
-      p.iterate(t1);
-      print("--- double t1a = t1 ---");
-      double t1a = t1;
-      p.isSameObj(t1a, "t1a", t1, "t1");
-
-      // reference behavior - copy on write
-      print("----------");
-      print("   String reference type:");
-      print("----------");
-      String t2 = "a string";
-      //p.showIdent(t2, "t2");
-      p.showType(t2, "t2");
-      
-      print("--- String t3 = t2 ---");
-      String t3 = t2;
-      //p.showIdent(t3, "t3");
-      p.showType(t3, "t3");
-      p.isSameObj(t3,"t3",t2,"t2");
-
-      print("--- t3 += \", ok\" ---");
-      t3 += ", ok";
-      p.showType(t3, "t3");
-      p.isSameObj(t3,"t3",t2,"t2");
-      
-      
-      print("--- t3 = \"another string\" ---");
-      t3 = "another string";
-      p.isSameObj(t3,"t3",t2,"t2");
-
-      //   
-      print("----------");
-      print("   Object obj1 = new Object()");
-      print("----------");
-      Object obj1 = new Object();
-      // p.showIdent(obj, "obj");
-      p.showType(obj1, "obj");
-      p.iterate(obj1);
-      print("--- Object obj2 = obj1 ---");
-      Object obj2 = obj1;
-      p.isSameObj(obj2, "obj2", obj1, "obj1");
-
-      print("----------");
-      print("   T4 t4 - new T4()");
-      print("----------");
-      T4 t4 = new T4();
-      t4.I = 3;
-      t4.D = 3.1415927;
-      t4.C = 'z';
-      t4.printSelf("t4");
-      // p.showIdent(t4, "t4");
-      p.showType(t4, "t4");
-      p.iterate(t4);
-
-      print("--- t4a = t4 ---");
-      T4 t4a = t4;
-      t4a.printSelf("t4a");
-      t4.printSelf("t4");
-      p.isSameObj(t4a,"t4a",t4,"t4");
-      p.showType(t4a, "t4a");
-
-      print("--- t4a.C = 'q' ---");
-      t4a.C = 'q';
-      t4a.printSelf("t4a");
-      // p.showIdent(t4a, "t4a");
-      t4.printSelf("t4");
-      p.isSameObj(t4, "t4", t4a, "t4a");
-
-      print("----------");
-      print("   T5 t5 - new T5()");
-      print("----------");
-      T5 t5 = new T5();
-      t5.S = "SomeString";
-      t5.printSelf("t5");
-      p.showType(t5, "t5");
-
-      print("--- T t5a = t5 ---");
-      T5 t5a = t5;
-      t5a.printSelf("t5a");
-      p.isSameObj(t5a, "t5a", t5, "t5");
-      p.isSameObj(t5a.S, "t5a.S", t5.S, "t5.S");
-
-      Console.WriteLine("\nThat's all Folks!\n");
+    public static void showNote(string s) {
+      Console.WriteLine("--- Note: ---\n  {0}", s);
     }
   }
   /*-- 
