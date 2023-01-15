b9686e80f6ead9b848fbca8c92b64557f1985710
Jim Fawcett <fawcettjim@outlook.com>
2023-01-14 19:36:17 -0600

    updated Rust and Python code

---
+++ Rust/rust_hello_data/target/debug/deps/rust_hello_data-f019d06a4f2afb7d.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\Bits\Rust\rust_hello_data\target\debug\deps\rust_hello_data-f019d06a4f2afb7d.rmeta: src\main.rs
+
+C:\github\JimFawcett\Bits\Rust\rust_hello_data\target\debug\deps\rust_hello_data-f019d06a4f2afb7d.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust_hello_data/target/debug/rust_hello_data.d
@@ -0,0 +1,1 @@
+C:\github\JimFawcett\Bits\Rust\rust_hello_data\target\debug\rust_hello_data.exe: C:\github\JimFawcett\Bits\Rust\rust_hello_data\src\main.rs
