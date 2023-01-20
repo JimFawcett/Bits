125a53e44f927a71e2e40afc1b34fab9089c7b80
Jim Fawcett <fawcettjim@outlook.com>
2023-01-19 20:43:06 -0600

    Developed code for Rust Bits_Objects. Have'nt installed yet

---
+++ Rust/rust_hello_objects/target/debug/deps/rust_hello_objects.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\Bits\Rust\rust_hello_objects\target\debug\deps\rust_hello_objects.exe: src\main.rs
+
+C:\github\JimFawcett\Bits\Rust\rust_hello_objects\target\debug\deps\rust_hello_objects.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust_hello_objects/target/debug/rust_hello_objects.d
@@ -0,0 +1,1 @@
+C:\github\JimFawcett\Bits\Rust\rust_hello_objects\target\debug\rust_hello_objects.exe: C:\github\JimFawcett\Bits\Rust\rust_hello_objects\src\main.rs
