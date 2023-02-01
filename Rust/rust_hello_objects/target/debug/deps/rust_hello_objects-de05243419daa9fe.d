95895fc89a89af550f62c863ae70a6018dce351e
Jim Fawcett <fawcettjim@outlook.com>
2023-01-31 20:47:29 -0600

    simplified Cpp_Objects

---
+++ Rust/rust_hello_objects/target/debug/deps/rust_hello_objects-de05243419daa9fe.d
@@ -0,0 +1,5 @@
+c:\github\JimFawcett\Bits\Rust\rust_hello_objects\target\debug\deps\rust_hello_objects-de05243419daa9fe.rmeta: src\main.rs
+
+c:\github\JimFawcett\Bits\Rust\rust_hello_objects\target\debug\deps\rust_hello_objects-de05243419daa9fe.d: src\main.rs
+
+src\main.rs:
