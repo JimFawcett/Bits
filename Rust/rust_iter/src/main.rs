d3189d4fddd899244dd7252ac68f1b933533d0b4
Jim Fawcett <fawcettjim@outlook.com>
2023-03-08 23:22:16 -0600

    added comments and more tests

--- Rust/rust_iter/src/main.rs
+++ Rust/rust_iter/src/main.rs
@@ -1,12 +1,98 @@
 use std::fmt::*;
+use std::cmp::*;
+use std::collections::*;
 
 /*-----------------------------------------------
+  Simplest case - displays Vector with
+  generic type.
+  - very close to C++ version
+  - see comments for simple_indexer, below
+*/
+fn whiler<T: Debug>(v:&Vec<T>) {
+    let mut i = 0;
+    while i < v.len() {
+        print!("{:?} ", v[i]);
+        i += 1;
+    }
+    println!();
+}
+/*-----------------------------------------------
+  Illustrates indexing
+  - Not idiomatic, but safe and correct.
+  - Works with any collection with fixed size
+    elements.
+  - Won't work with String or str inputs; they
+    are collections of utf8 chars which vary
+    in size from 1 to 4 bytes.
+  - It is rare for idomatic Rust code to use
+    indexing. 
+  - &[T] is slice type
+*/
+#[allow(clippy::needless_range_loop)]
+fn simple_indexer<T:Debug>(s:&[T]) {
+    let max = s.len();
+    /* 0..max is range iterator */
+    for i in 0..max {
+        print!("{:?} ", s[i]);
+    }
+    println!();
+    /*---------------------------
+      clippy prefers:
+      for item in s.iter().take(max) {
+        print!("{item} ");
+      }
+    */
+}
+/*-----------------------------------------------
+  Iterates over a sub-range of the slice s
+  - works with any collection with fixed size
+    elements that converts to slice.
+*/
+fn sub_range_indexer<T:Debug>(
+    s:&[T], mut lower:usize, mut upper:usize
+) {
+    lower = max(0, lower);
+    upper = min(s.len(), upper);
+    if lower <= upper {
+      for i in lower..upper {
+        print!("{:?} ", s[i]);
+      }
+    }
+    println!();
+}
+/*-----------------------------------------------
+  Iterates over slice s without indexing
+  - Works with any collection with fixed size
+    elements if they provide a Deref to slice,
+    e.g. array, Vector
+*/
+fn simple_looper<T:Debug>(s:&[T]) {
+    let mut iter = s.iter();
+    loop {
+        let item = iter.next();
+        match item {
+            Some(val) => print!("{val:?} "),
+            None => break
+        }
+    }
+    println!();
+}
+/*-----------------------------------------------
   fn looper prints out a comma separated list of
-  the Collection c's items.
+  Collection<I>'s items.
   - where clause requires C to implement 
-    into_iterator(). 
+    IntoIterator trait. 
+  - C is type of collection, I is type of C's items
   - 'a annotation says that c and its items last 
     at least as long as the function call.
+  - Accepts any collection type that implements
+    IntoIterator trait, e.g., array, slice, Vector, ...
+  - looper can't accept string or string ref
+    because String does not implement IntoIterator
+  - That's because String provides two iterators,
+    chars() to iterate multibyte chars and bytes()
+    to iterate over bytes.
+    https://stackoverflow.com/questions/49962611/why-does-str-not-implement-intoiterator
 */
 fn looper<C: Debug, I: Debug>(c:&C) 
     where for<'a> &'a C: IntoIterator<Item = &'a I>
@@ -14,8 +100,7 @@ fn looper<C: Debug, I: Debug>(c:&C)
     /* convert c into Vec to get len() method */
     let iter = c.into_iter();
     let v:Vec<_> = iter.collect();
-
-    let mut iter = v.iter();
+    let mut iter = v.iter();  // shadowing
     let mut count = 0;
     loop {
         let item = iter.next();
@@ -33,7 +118,7 @@ fn looper<C: Debug, I: Debug>(c:&C)
   similar to looper but uses for loop and erases
   last comma
 */
-fn forlooper<C: Debug, I: Debug>(c:&C) 
+fn for_looper<C: Debug, I: Debug>(c:&C) 
     where for<'a> &'a C: IntoIterator<Item = &'a I>
 {
     /* build string of comma separated values */
@@ -65,18 +150,6 @@ fn ranger<T: Debug>(iter: &mut T)
     }
     println!();
 }
-/*-----------------------------------------------
-  Simplest case - displays known collection
-  with generic type
-*/
-fn whiler<T: Debug>(v:&Vec<T>) {
-    let mut i = 0;
-    while i < v.len() {
-        print!("{:?} ", v[i]);
-        i += 1;
-    }
-    println!();
-}
 fn main() {
     println!("----------------------------------");
     println!("--- Demonstrate Rust Iteration ---");
@@ -84,23 +157,56 @@ fn main() {
     let s = &[1usize, 2, 3, 4, 3, 2, 1];
     println!("slice s = {s:?}");
     println!("s[2] = {:?}", s[2usize]);
-    /* works for slice */
+    
+    println!("\nwhiler displays Vec<T>");
+    let v = vec![1, 6, 2, 5, 3, 4];
+    whiler(&v);
+
+    println!("simple_indexer displays slice");
+    simple_indexer(s);
+    println!("simple_indexer displays vector");
+    let v = vec![5, 4, 3, 2, 1, 0];
+    simple_indexer(&v);
+    println!("simple_indexer displays string bytes");
+    let str1:&str = "a string";
+    simple_indexer(str1.as_bytes());
+    
+    println!("sub_range_indexer displays slice");
+    sub_range_indexer(s, 2, 5);
+    
+    println!("simple_looper displays slice");
+    simple_looper(s);
+    
     println!("looper displays slice:");
     looper(s);
-    /* works for array */
     println!("looper displays array");
     let a = [1, 2, 3];
     looper(&a);
-    /* works for Vec too */
-    println!("forlooper displays vector:");
+    println!("looper displays VecDeque");
+    let vecdeq = VecDeque::from([4, 3, 2, 0, -1]);
+    looper(&vecdeq);
+    
+    println!("for_looper displays slice:");
+    for_looper(s);
+    println!("for_looper displays vector:");
     let t = s.to_vec();
-    forlooper(&t);
-
-    println!("ranger displays range");
+    for_looper(&t);
+    println!("for_looper displays VecDeque");
+    for_looper(&vecdeq);
+    /*------------------------------------------- 
+      for_looper can't accept String or &str
+      because they do not implement IntoIterator
+      https://stackoverflow.com/questions/49962611/why-does-str-not-implement-intoiterator
+    */
+    println!("ranger displays string:");
+    let str = "a literal string".to_string();
+    ranger(&mut str.chars());
+    println!("ranger displays values in range");
     ranger(&mut (0..10));
-    println!("ranger displays results of iteration");
+    println!("ranger accepts Vector iterator");
     ranger(&mut t.iter());
-    println!("whiler displays Vec<T>");
-    whiler(&t);
+    println!("ranger accepts VecDeque iterator");
+    ranger(&mut vecdeq.iter());
+
     println!("\nThat's all folks!\n");
 }
